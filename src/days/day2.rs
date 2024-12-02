7cc96f976b6c3d20dd491ac7aa8c008ed58914e2
BuzzWP <buzzwpendarvis@gmail.com>
2024-12-02 07:19:17 -0800

    just ran cargo fmt on it lol

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -5,13 +5,12 @@ pub fn run(input: &String) {
 
     // Part 1
     // get reports as an iter of Vec<i32>
-    let reports = input.lines()
-        .map(|l| 
-            l.split(|c: char| c.is_whitespace())
-                .map(|n| n.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>()
-        );
-    
+    let reports = input.lines().map(|l| {
+        l.split(|c: char| c.is_whitespace())
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>()
+    });
+
     // For each report, return true if safe and false if not safe
     let safe = reports.clone().map(|r| {
         // unsafe if first two elements are the same
@@ -22,8 +21,11 @@ pub fn run(input: &String) {
         let increasing = r[0] < r[1];
 
         // check if all entries are also increasing/decreasing, and if they're within the right range
-        for level in 0..r.len()-1 {
-            if (r[level] == r[level+1]) || (increasing ^ (r[level] < r[level+1])) || (r[level].abs_diff(r[level+1]) > 3) {
+        for level in 0..r.len() - 1 {
+            if (r[level] == r[level + 1])
+                || (increasing ^ (r[level] < r[level + 1]))
+                || (r[level].abs_diff(r[level + 1]) > 3)
+            {
                 return false;
             }
         }
@@ -32,30 +34,37 @@ pub fn run(input: &String) {
     });
 
     let safe2 = reports.clone().map(|r| {
-        // Create iter of each report with different elements removed 
+        // Create iter of each report with different elements removed
         // (including the initial list which is probably not neccessary)
-        let versions = (0..r.len()).map(|i| {
-            let mut removed = r.clone();
-            removed.remove(i);
-            removed
-        }).chain(Some(r.clone()));
+        let versions = (0..r.len())
+            .map(|i| {
+                let mut removed = r.clone();
+                removed.remove(i);
+                removed
+            })
+            .chain(Some(r.clone()));
 
         // verify each one until one returns true
         // if none return true, return false.
-        versions.map(|r: Vec<i32>| {
-            if r[0] == r[1] {
-                return false;
-            }
-            let increasing = r[0] < r[1];
-    
-            for level in 0..r.len()-1 {
-                if (r[level] == r[level+1]) || (increasing ^ (r[level] < r[level+1])) || (r[level].abs_diff(r[level+1]) > 3) {
+        versions
+            .map(|r: Vec<i32>| {
+                if r[0] == r[1] {
                     return false;
                 }
-            }
-    
-            true
-        }).any(|b| b)
+                let increasing = r[0] < r[1];
+
+                for level in 0..r.len() - 1 {
+                    if (r[level] == r[level + 1])
+                        || (increasing ^ (r[level] < r[level + 1]))
+                        || (r[level].abs_diff(r[level + 1]) > 3)
+                    {
+                        return false;
+                    }
+                }
+
+                true
+            })
+            .any(|b| b)
     });
 
     // count true/false i.e. safe/unsafe reports
