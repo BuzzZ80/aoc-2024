3ef2640776ef2fdfa8f885aa2c89d40edee05a74
BuzzWP <buzzwpendarvis@gmail.com>
2024-12-03 02:08:02 -0800

    day 3

---
+++ src/days/day3.rs
@@ -0,0 +1,27 @@
+use regex::Regex;
+
+pub fn run(input: &String) {
+    println!("--- Day 3: Mull It Over ---");
+
+    // spaghetti
+    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)|do\(\)()()|don't\(\)()()").unwrap();
+
+    let mut accumulator_1: u64 = 0;
+    let mut accumulator_2: u64 = 0;
+    let mut enabled = true;
+    re.captures_iter(input).for_each(|c| {
+        let (m, [n1, n2]) = c.extract::<2>();
+        if matches!(m, "do()") {
+            enabled = true;
+        } else if matches!(m, "don't()") {
+            enabled = false;
+        } else {
+            accumulator_1 += n1.parse::<u64>().unwrap() * n2.parse::<u64>().unwrap();
+            accumulator_2 +=
+                n1.parse::<u64>().unwrap() * n2.parse::<u64>().unwrap() * enabled as u64;
+        }
+    });
+
+    println!("Star 1: {accumulator_1}");
+    println!("Star 2: {accumulator_2}");
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,10 +1,12 @@
 mod day1;
 mod day2;
+mod day3;
 
 pub fn run(day: i8, input: &String) -> Result<(), String> {
     match day {
         1 => day1::run(input),
         2 => day2::run(input),
+        3 => day3::run(input),
         d => Err(format!("Invalid day `{d}`"))?,
     }
     Ok(())
