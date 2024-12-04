9294d634e4a3083929f831c3e3f0c4478bfb4d5f
BuzzWP <buzzwpendarvis@gmail.com>
2024-12-03 22:52:48 -0800

    less horible

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -5,75 +5,36 @@ pub fn run(input: &String) {
         .collect();
 
     // list of s's in the text
-    let mut s_list: Vec<(i32, i32)> = Vec::new();
+    let mut s_list: Vec<(usize, usize)> = Vec::new();
 
     // find every s in the grid
     for row in 0..grid.len() {
         for col in 0..grid[row].len() {
             if grid[row][col] == b'S' {
-                s_list.push((row as i32, col as i32));
+                s_list.push((row, col));
             }
         }
     }
 
     let mut accumulator = 0;
     for s in s_list {
-        // left check
-        accumulator += "XMA"
-            .bytes()
-            .enumerate()
-            .map(|(a, b)| (a as i32, b))
-            .map(|(i, v)| check_char((s.0, s.1 - 3 + i), v, &grid))
-            .all(|b| b == true) as i32;
-        // up_left check
-        accumulator += "XMA"
-            .bytes()
-            .enumerate()
-            .map(|(a, b)| (a as i32, b))
-            .map(|(i, v)| check_char((s.0 + 3 - i, s.1 - 3 + i), v, &grid))
-            .all(|b| b == true) as i32;
-        // down_left check
-        accumulator += "XMA"
-            .bytes()
-            .enumerate()
-            .map(|(a, b)| (a as i32, b))
-            .map(|(i, v)| check_char((s.0 - 3 + i, s.1 - 3 + i), v, &grid))
-            .all(|b| b == true) as i32;
-        // up check
-        accumulator += "XMA"
-            .bytes()
-            .enumerate()
-            .map(|(a, b)| (a as i32, b))
-            .map(|(i, v)| check_char((s.0 + 3 - i, s.1), v, &grid))
-            .all(|b| b == true) as i32;
-        // down check
-        accumulator += "XMA"
-            .bytes()
-            .enumerate()
-            .map(|(a, b)| (a as i32, b))
-            .map(|(i, v)| check_char((s.0 - 3 + i, s.1), v, &grid))
-            .all(|b| b == true) as i32;
-        // right check
-        accumulator += "XMA"
-            .bytes()
-            .enumerate()
-            .map(|(a, b)| (a as i32, b))
-            .map(|(i, v)| check_char((s.0, s.1 + 3 - i), v, &grid))
-            .all(|b| b == true) as i32;
-        // up right check
-        accumulator += "XMA"
-            .bytes()
-            .enumerate()
-            .map(|(a, b)| (a as i32, b))
-            .map(|(i, v)| check_char((s.0 + 3 - i, s.1 + 3 - i), v, &grid))
-            .all(|b| b == true) as i32;
-        // down right check
-        accumulator += "XMA"
-            .bytes()
-            .enumerate()
-            .map(|(a, b)| (a as i32, b))
-            .map(|(i, v)| check_char((s.0 - 3 + i, s.1 + 3 - i), v, &grid))
-            .all(|b| b == true) as i32;
+        for branch in (0..=8).filter(|n| *n != 4) {
+            let first: i32 = branch % 3 - 1; // ranges from -1 to 1
+            let second: i32 = branch / 3 - 1; // ranges from -1 to 1
+
+            accumulator += "AMX"
+                .bytes()
+                .enumerate()
+                .map(|(i, v)| {
+                    let i = i as i32 + 1;
+                    let coords = (
+                        (s.0 as i32 + i * first) as usize,
+                        (s.1 as i32 + i * second) as usize,
+                    );
+                    check_char(coords, v, &grid)
+                })
+                .all(|b| b == true) as i32;
+        }
     }
 
     println!("Star 1: {}", accumulator);
@@ -104,7 +65,7 @@ pub fn run(input: &String) {
     println!("Star 2: {}", accumulator);
 }
 
-fn check_char(coord: (i32, i32), val: u8, grid: &Vec<Vec<u8>>) -> bool {
+fn check_char(coord: (usize, usize), val: u8, grid: &Vec<Vec<u8>>) -> bool {
     if let Some(Some(read_value)) = grid
         .get(coord.0 as usize)
         .map(|col| col.get(coord.1 as usize))
