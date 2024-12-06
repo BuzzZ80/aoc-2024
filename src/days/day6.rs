8a813b89a9bb629169c238b98fa1e8eb70f36358
BuzzWP <buzzwpendarvis@gmail.com>
2024-12-05 23:42:20 -0800

    cleaned up solution

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,10 +1,12 @@
 use std::collections::HashSet;
 
 pub fn run(input: &String) {
-    let grid_width = input.lines().next().expect("Wrong input?").len();
-    let grid_height = input.lines().collect::<Vec<_>>().len();
+    // parse input
+    // get dimensions of grid
+    let grid_width = input.lines().next().expect("Wrong input?").len() as i32;
+    let grid_height = input.lines().collect::<Vec<_>>().len() as i32;
 
-    let mut guard_dir: i32 = 0;
+    // locate `^` symbol
     let initial_guard_pos: (i32, i32) = input
         .lines()
         .enumerate()
@@ -16,8 +18,8 @@ pub fn run(input: &String) {
         })
         .next()
         .expect("Wrong input?");
-    let mut guard_pos = initial_guard_pos;
-
+    
+    // locate all `#` symbols and place them in a hash set
     let obstructions: HashSet<(i32, i32)> = input
         .lines()
         .enumerate()
@@ -29,18 +31,42 @@ pub fn run(input: &String) {
         })
         .collect();
 
-    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    // part 1 solution
+    let path = get_full_path(((initial_guard_pos), 0), obstructions.clone(), grid_width, grid_height).expect("input contains loop");
+    println!("Star 1: {}", path.len());
 
+    // part 2 solution
+    // try placing an obstacle in every place visited by the guard
+    let configurations: i32 = path.iter()
+        .filter(|(obs_x, obs_y)| !(*obs_x == initial_guard_pos.0 && *obs_y == initial_guard_pos.1))
+        .map(|(obs_x, obs_y)| {
+            let mut new_obstructions = obstructions.clone();
+            new_obstructions.insert((*obs_x, *obs_y));
+            get_full_path(((initial_guard_pos), 0), new_obstructions, grid_width, grid_height).is_none() as i32
+        })
+        .sum();
+    println!("Star 2: {}", configurations);
+}
+
+/// returns none if loop is detected
+fn get_full_path(start_state: ((i32, i32), i32), obstructions: HashSet<(i32, i32)>, grid_width: i32, grid_height: i32) -> Option<HashSet<(i32, i32)>> {
+    let (mut guard_pos, mut guard_dir) = start_state;
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    let mut guard_prev_states: HashSet<((i32, i32), i32)> = HashSet::new();
     loop {
         if guard_pos.0 < 0
             || guard_pos.1 < 0
             || guard_pos.0 >= grid_width as i32
             || guard_pos.1 >= grid_height as i32
         {
-            break;
+            break Some(visited);
+        } else if guard_prev_states.contains(&(guard_pos, guard_dir)){
+            break None;
         }
 
         visited.insert(guard_pos);
+        guard_prev_states.insert((guard_pos, guard_dir));
+        
 
         let obstructed = match guard_dir {
             0 => obstructions.contains(&(guard_pos.0, guard_pos.1 - 1)),
@@ -62,54 +88,4 @@ pub fn run(input: &String) {
             }
         }
     }
-
-    println!("Star 1: {}", visited.len());
-
-    // part 2
-    let mut count2 = 0;
-    for (obs_x, obs_y) in visited {
-        guard_dir = 0;
-        guard_pos = initial_guard_pos;
-        if obs_x == guard_pos.0 && obs_y == guard_pos.1 { continue; }
-        let mut guard_prev_states: HashSet<((i32, i32), i32)> = HashSet::new();
-        let mut new_obstructions = obstructions.clone();
-        new_obstructions.insert((obs_x, obs_y));
-        let causes_loop = loop {
-            if guard_pos.0 < 0
-                || guard_pos.1 < 0
-                || guard_pos.0 >= grid_width as i32
-                || guard_pos.1 >= grid_height as i32
-            {
-                break false;
-            } else if guard_prev_states.contains(&(guard_pos, guard_dir)){
-                break true;
-            }
-    
-            guard_prev_states.insert((guard_pos, guard_dir));
-            
-    
-            let obstructed = match guard_dir {
-                0 => new_obstructions.contains(&(guard_pos.0, guard_pos.1 - 1)),
-                1 => new_obstructions.contains(&(guard_pos.0 + 1, guard_pos.1)),
-                2 => new_obstructions.contains(&(guard_pos.0, guard_pos.1 + 1)),
-                3 => new_obstructions.contains(&(guard_pos.0 - 1, guard_pos.1)),
-                _ => panic!("you goofed it lol"),
-            };
-    
-            if obstructed {
-                guard_dir = (guard_dir + 1) % 4;
-            } else {
-                guard_pos = match guard_dir {
-                    0 => (guard_pos.0, guard_pos.1 - 1),
-                    1 => (guard_pos.0 + 1, guard_pos.1),
-                    2 => (guard_pos.0, guard_pos.1 + 1),
-                    3 => (guard_pos.0 - 1, guard_pos.1),
-                    _ => panic!("you goofed it lol"),
-                }
-            }
-        };  
-
-        count2 += causes_loop as i32;
-    }
-    println!("Star 2: {}", count2);
 }
