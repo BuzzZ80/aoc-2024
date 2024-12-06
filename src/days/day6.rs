9cf8f3e7c0cf41f2a8d62e6d4576f50dc6a02ec7
BuzzWP <buzzwpendarvis@gmail.com>
2024-12-05 23:09:29 -0800

    o_o it was as simple as only checking visited tiles...

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -68,51 +68,49 @@ pub fn run(input: &String) {
     // part 2 ;-;;;;
     let timer = std::time::Instant::now();
     let mut count2 = 0;
-    for obs_x in 0..grid_width as i32 {
-        for obs_y in 0..grid_height as i32 {
-            guard_dir = 0;
-            guard_pos = initial_guard_pos;
-            if obs_x == guard_pos.0 && obs_y == guard_pos.1 { continue; }
-            let mut guard_prev_states: HashSet<((i32, i32), i32)> = HashSet::new();
-            let mut new_obstructions = obstructions.clone();
-            new_obstructions.insert((obs_x, obs_y));
-            let causes_loop = loop {
-                if guard_pos.0 < 0
-                    || guard_pos.1 < 0
-                    || guard_pos.0 >= grid_width as i32
-                    || guard_pos.1 >= grid_height as i32
-                {
-                    break false;
-                } else if guard_prev_states.contains(&(guard_pos, guard_dir)){
-                    break true;
-                }
-        
-                guard_prev_states.insert((guard_pos, guard_dir));
-                
-        
-                let obstructed = match guard_dir {
-                    0 => new_obstructions.contains(&(guard_pos.0, guard_pos.1 - 1)),
-                    1 => new_obstructions.contains(&(guard_pos.0 + 1, guard_pos.1)),
-                    2 => new_obstructions.contains(&(guard_pos.0, guard_pos.1 + 1)),
-                    3 => new_obstructions.contains(&(guard_pos.0 - 1, guard_pos.1)),
+    for (obs_x, obs_y) in visited {
+        guard_dir = 0;
+        guard_pos = initial_guard_pos;
+        if obs_x == guard_pos.0 && obs_y == guard_pos.1 { continue; }
+        let mut guard_prev_states: HashSet<((i32, i32), i32)> = HashSet::new();
+        let mut new_obstructions = obstructions.clone();
+        new_obstructions.insert((obs_x, obs_y));
+        let causes_loop = loop {
+            if guard_pos.0 < 0
+                || guard_pos.1 < 0
+                || guard_pos.0 >= grid_width as i32
+                || guard_pos.1 >= grid_height as i32
+            {
+                break false;
+            } else if guard_prev_states.contains(&(guard_pos, guard_dir)){
+                break true;
+            }
+    
+            guard_prev_states.insert((guard_pos, guard_dir));
+            
+    
+            let obstructed = match guard_dir {
+                0 => new_obstructions.contains(&(guard_pos.0, guard_pos.1 - 1)),
+                1 => new_obstructions.contains(&(guard_pos.0 + 1, guard_pos.1)),
+                2 => new_obstructions.contains(&(guard_pos.0, guard_pos.1 + 1)),
+                3 => new_obstructions.contains(&(guard_pos.0 - 1, guard_pos.1)),
+                _ => panic!("you goofed it lol"),
+            };
+    
+            if obstructed {
+                guard_dir = (guard_dir + 1) % 4;
+            } else {
+                guard_pos = match guard_dir {
+                    0 => (guard_pos.0, guard_pos.1 - 1),
+                    1 => (guard_pos.0 + 1, guard_pos.1),
+                    2 => (guard_pos.0, guard_pos.1 + 1),
+                    3 => (guard_pos.0 - 1, guard_pos.1),
                     _ => panic!("you goofed it lol"),
-                };
-        
-                if obstructed {
-                    guard_dir = (guard_dir + 1) % 4;
-                } else {
-                    guard_pos = match guard_dir {
-                        0 => (guard_pos.0, guard_pos.1 - 1),
-                        1 => (guard_pos.0 + 1, guard_pos.1),
-                        2 => (guard_pos.0, guard_pos.1 + 1),
-                        3 => (guard_pos.0 - 1, guard_pos.1),
-                        _ => panic!("you goofed it lol"),
-                    }
                 }
-            };  
+            }
+        };  
 
-            count2 += causes_loop as i32;
-        }
+        count2 += causes_loop as i32;
     }
     println!("Star 2: {} ({} seconds)", count2, timer.elapsed().as_secs());
 }
