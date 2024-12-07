42ee2d6163d6cd6ff7ba4ea2ebc61fd963410e36
BuzzWP <buzzwpendarvis@gmail.com>
2024-12-07 04:18:31 -0800

    changed day 7 more

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -15,16 +15,24 @@ pub fn run(input: &String) {
     let timer2 = std::time::Instant::now();
     let sum: i64 = equations_iter
         .clone()
-        .filter_map(|eq| if eq_is_valid_2(&eq) { Some(eq[0]) } else { None })
+        .filter_map(|eq| {
+            if eq_is_valid_2(&eq) {
+                Some(eq[0])
+            } else {
+                None
+            }
+        })
         .sum();
     println!("Star 2: {} ({} ms)", sum, timer2.elapsed().as_millis());
 }
 
-fn eq_is_valid(eq: &Vec<i64>) -> bool {
-    let mut eq = eq.iter();
-    let ans = *eq.next().expect("Wrong input?");
-    let first = *eq.next().expect("Wrong input?");
-    let len = eq.clone().count() as u32;
+fn eq_is_valid(eq: &[i64]) -> bool {
+    let ans = eq[0];
+    let first = eq[1];
+    let len = eq.len() as u32 - 2;
+
+    let eq = eq[2..].iter();
+
     let mut found = false;
     for operations in 0..2_i64.pow(len) {
         let result = eq.clone().enumerate().fold(first, |accumulator, (i, n)| {
@@ -42,27 +50,25 @@ fn eq_is_valid(eq: &Vec<i64>) -> bool {
     found
 }
 
-fn eq_is_valid_2(eq: &Vec<i64>) -> bool {
-    let mut eq = eq.iter();
-    let ans = *eq.next().expect("Wrong input?");
-    let first = *eq.next().expect("Wrong input?");
-    let len = eq.clone().count() as u32;
+fn eq_is_valid_2(eq: &[i64]) -> bool {
+    let ans = eq[0];
+    let first = eq[1];
+    let len = eq.len() as u32 - 2;
+
+    let eq = eq[2..].iter();
+
     let mut found = false;
     for operations in 0..3_i64.pow(len) {
         let result = eq.clone().enumerate().fold(first, |accumulator, (i, n)| {
             let op = (operations / 3_i64.pow(i as u32)) % 3;
-            match op {
-                0 => accumulator + n,
-                1 => accumulator * n,
-                2 => append(accumulator, *n),
-                _ => panic!()
-            }
+            unsafe {[i64::unchecked_add, i64::unchecked_mul, append][op as usize](accumulator, *n)}
         });
         if result == ans {
             found = true;
             break;
         }
     }
+
     found
 }
 
